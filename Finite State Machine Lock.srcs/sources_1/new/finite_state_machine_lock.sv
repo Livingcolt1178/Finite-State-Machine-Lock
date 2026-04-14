d0e9c6ff13e09cbd9de91ceaa5a3286b9e2ea787
Nicholas <nrbramhall1@gmail.com>
2026-04-14 12:38:30 -0400

    initilizing repository

---
+++ Finite State Machine Lock.srcs/sources_1/new/finite_state_machine_lock.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2026 12:34:14 PM
+// Design Name: 
+// Module Name: finite_state_machine_lock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module finite_state_machine_lock(
+
+    );
+endmodule
